5867b4cd11ca7c80dbceacd60a92e2a5d46b81cd
Elías <elias.ludviksson@gmail.com>
2024-01-21 17:05:57 +0000

    óþarfa <a> í fót

--- src/app/fotur.tsx
+++ src/app/fotur.tsx
@@ -4,20 +4,18 @@ import styles from "./fotur.module.scss"
 export function Fotur() {
 	return <>
 		<footer>
-			<a>
-				<figure className={styles.fotur}>
-					<Image
-						src="/foot.svg"
-						alt="Elías Lúðviksson/Elli.vip logo"
-						width="790"
-						height="790"
-						className={styles.fotur__mynd}
-					/>
-					<figcaption className={styles.fotur__caption}>
-						Elías Lúðvíksson
-					</figcaption>
-				</figure>
-			</a>
+			<figure className={styles.fotur}>
+				<Image
+					src="/foot.svg"
+					alt="Elías Lúðviksson/Elli.vip logo"
+					width="790"
+					height="790"
+					className={styles.fotur__mynd}
+				/>
+				<figcaption className={styles.fotur__caption}>
+					Elías Lúðvíksson
+				</figcaption>
+			</figure>
 		</footer>
 	</>
 }
