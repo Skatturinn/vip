7ada93058a6cdcaa28253393df931400ed28a6cf
Elías <elias.ludviksson@gmail.com>
2024-02-03 23:42:45 +0000

    um mig hlekk setur í fót

--- src/app/fotur.tsx
+++ src/app/fotur.tsx
@@ -1,21 +1,24 @@
 import Image from "next/image";
 import styles from "./fotur.module.scss"
-
+import Link from "next/link";
 export function Fotur() {
 	return <>
 		<footer>
-			<figure className={styles.fotur}>
-				<Image
-					src="/foot.svg"
-					alt="Elías Lúðviksson/Elli.vip logo"
-					width="790"
-					height="790"
-					className={styles.fotur__mynd}
-				/>
-				<figcaption className={styles.fotur__caption}>
-					Elías Lúðvíksson
-				</figcaption>
-			</figure>
+			<Link
+				href={'/elias-ludviksson'}>
+				<figure className={styles.fotur}>
+					<Image
+						src="/foot.svg"
+						alt="Elías Lúðviksson/Elli.vip logo"
+						width="790"
+						height="790"
+						className={styles.fotur__mynd}
+					/>
+					<figcaption className={styles.fotur__caption}>
+						Elías Lúðvíksson
+					</figcaption>
+				</figure>
+			</Link>
 		</footer>
 	</>
 }
