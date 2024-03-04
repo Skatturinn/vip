e70f1f2d0da37d979a4d413ca58b06ae9ce5a78a
Elías <elias.ludviksson@gmail.com>
2024-03-04 04:17:15 +0000

    bua til timabundnar sidur fyrir skil i vel608g

---
+++ src/app/verkefni/vel608g/verkefni3/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link"
+import Image from "next/image";
+import styles from "../verkefni.module.scss"
+
+export default function Page() {
+	return <>
+		<p>VÉL608G</p>
+		Sæll hafliði ef þú sérð þetta, þá er síðan ennþá í vinnslu, mátt endilega ýta á eftir mér, fer í þetta við næsta tækifæri</>
+}
