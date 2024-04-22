87d2e7c59d76aa778fc775c17cc1b85a8904894c
Elías <elias.ludviksson@gmail.com>
2024-04-22 16:55:06 +0000

    minn verkþattur

---
+++ src/app/verkefni/vel608g/verkþattur/page.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link"
+import Image from "next/image";
+import styles from "/src/app/verkefni/verkefni.module.scss"
+
+export default function Page() {
+	return <p>Ég skrifa um mitt framlag hérma</p>
+}
